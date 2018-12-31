4351449a5d7cb4158a4a05622dd2fd0556e1273c
godcong <jumbycc@163.com>
2019-01-01 07:22:50 +0800

    add cluster add

--- src/app/cluster/cluster.service.ts
+++ src/app/cluster/cluster.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClusterService {
+  http: HttpClient;
 
-  constructor() { }
+  requestInit() {
+    return this.http.get("http://localhost:7758/v0/heartbeat");
+  }
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
 }
--- src/app/cluster/list/list.component.ts
+++ src/app/cluster/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClusterService} from "../cluster.service";
+import {HttpResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-list',
@@ -6,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  service: ClusterService;
 
-  constructor() { }
+  constructor(service: ClusterService) {
+    this.service = service;
+  }
+
+  getInit() {
+    let data: any;
+    this.service.requestInit()
+      .subscribe((v1: []) => {
+        // if (v1.body != null) {
+        console.log(v1["detail"]);
+        // }
+      });
+
+  }
 
   ngOnInit() {
   }
