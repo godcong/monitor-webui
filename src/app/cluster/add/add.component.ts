b94234a3dba139b7f03d7b59a5f57383eca6a9dc
godcong <jumbycc@163.com>
2019-01-02 15:42:36 +0800

    add cluster add to localStorage

--- src/app/cluster/add/add.component.ts
+++ src/app/cluster/add/add.component.ts
@@ -19,17 +19,40 @@ export class AddComponent implements OnInit {
     Port: 7758,
   };
 
+  host() {
+    if (this.form.Host !== '') {
+      return `http://${this.form.Host}:${this.form.Port}`;
+    }
+    return `http://localhost:${this.form.Port}`;
+  }
+
+  getHosts() {
+    const hosts = localStorage.getItem('hosts');
+    const jsonHost = JSON.parse(hosts);
+  }
+
   postInit() {
-    this.service.postInit({
+    let localHost = [];
+    this.service.postInit(this.host(), {
       'CLUSTER_SECRET': '5c2defbd5ea3f8806cc0aee8cf005219271a30d1be8b3312434fb814f9f9b768',
     }).subscribe((data: IpfsResponse) => {
       console.log(data);
-      if (data.Code === 0) {
-        hosts = localStorage.getItem('hosts');
-        console.log(hosts);
-        jsonHost = JSON.parse(hosts);
-        jsonHost[this.form.Host] = this.form;
-        localStorage.setItem('hosts', jsonHost.toJSON());
+      // if (data.Code === 0) {
+      // let hosts: InitForm[];
+      const hosts = localStorage.getItem('hosts');
+      console.log(hosts);
+      const key = this.form.Host === '' ? 'localhost' : this.form.Host;
+
+      if (hosts === null || hosts === '') {
+        localHost[key] = this.form;
+        console.log(localHost);
+        localStorage.setItem('hosts', JSON.stringify(localHost));
+
+      } else {
+        localHost = JSON.parse(hosts);
+        localHost[key] = this.form;
+        console.log(localHost);
+        localStorage.setItem('hosts', JSON.stringify(localHost));
       }
 
       history.go(-1);
--- src/app/cluster/cluster.service.ts
+++ src/app/cluster/cluster.service.ts
@@ -21,8 +21,8 @@ export class ClusterService {
     return this.http.get('http://localhost:7758/v0/heartbeat');
   }
 
-  postInit(postBody: any) {
-    return this.http.post('http://localhost:7758/v0/init', postBody, {
+  postInit(host: string, postBody: any) {
+    return this.http.post(host + '/v0/init', postBody, {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
       }
